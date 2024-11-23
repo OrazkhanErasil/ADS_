61cdbf8b8b110e17f8909dffaa3143185c1ea3c7
шаблон <orazkhanerasil@gmail.com>
2024-11-23 11:29:27 +0500

    Create Tea.java

---
+++ Модуль9/Decorator/Модуль9/Decorator/Tea.java
@@ -0,0 +1,11 @@
+public class Tea implements Beverage {
+@Override
+    public String getDescription() {
+        return "Tea";
+    }
+
+    @Override
+    public double cost() {
+        return 1.0;
+    }
+}
